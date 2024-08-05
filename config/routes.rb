2bced28ca875bf91cff98e2a8bdba243058c86fe
Immanuel Olaoye <olaoye939@gmail.com>
2024-08-05 15:17:06 -0500

    added bootstrap to application

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # GET /about
   get "about", to: "about#index"
+  root to: "main#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
